a448757c70a1b499711731952bc1a2f8581ea4b9
Oliverfk93916 <43701377+Oliverfk93916@users.noreply.github.com>
2020-12-16 21:03:37 +0000

    gets all data, no limit

--- src/strapi/getData.js
+++ src/strapi/getData.js
@@ -2,7 +2,7 @@
 import url from './URL'
 
 export default async() => {
-	const response = await fetch(`${url}/asset-data`).catch(error => console.error(error))
+	const response = await fetch(`${url}/asset-data?_limit=-1`).catch(error => console.error(error))
 	const assets = await response.json()
 	if(assets.error){
 		return null
