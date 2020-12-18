2e90a895743ba8496d2dd7f142dd0bd87a6a73fd
Oliverfk93916 <43701377+Oliverfk93916@users.noreply.github.com>
2020-12-18 15:07:30 +0000

    all controller parts added

--- src/pages/Stocktake.svelte
+++ src/pages/Stocktake.svelte
@@ -23,12 +23,15 @@
 <h1>Hello</h1>
 <h2>WIP</h2>
 
+<!-- CONTROLLER BUTTON -->
 <p style="margin-top: 20px;" class="d-grid gap-4 col-8 mx-auto" >
 	<a class="btn btn-outline-secondary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">HTC Vive Pro Controller</a>
 </p>
+<!-- CONTROLLER DROPDOWN -->
 <div class="row">
 	<div class="col-8 mx-auto">
     	<div class="collapse multi-collapse" id="multiCollapseExample1">
+    	<!-- CONTENT -->
     		<div class="input-group mb-3" >
 		 		<span class="input-group-text stocktakeSpan">Motherboard</span>
 	  			<input type="number" class="form-control stocktakeInput"required>
@@ -41,13 +44,38 @@
 		 		<span class="input-group-text stocktakeSpan">Trigger</span>
 	  			<input type="number" class="form-control stocktakeInput" required>
 	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Bottom Cover</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Battery</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Trackpad</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Top Button</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Side Button</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  		<div class="input-group mb-3">
+		 		<span class="input-group-text stocktakeSpan">Top Cover</span>
+	  			<input type="number" class="form-control stocktakeInput" required>
+	  		</div>
+	  	<!-- SUBMIT BUTTON -->
 			<div class="d-grid gap-4 col-6 mx-auto">
     			<button data-bs-toggle="collapse" href="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1" class="btn btn-dark stocktakeSubmit" type="submit">Submit form</button>
    			</div>
     	</div>
   	</div>
 </div>
-
+<!-- END OF CONTROLLER DROPDOWN -->
 <p class="d-grid gap-4 col-8 mx-auto">
 	<a class="btn btn-outline-secondary" data-bs-toggle="collapse" href="#multiCollapseExample2" role="button" aria-expanded="false" aria-controls="multiCollapseExample2">HTC Vive Pro Headset</a>
 </p>
